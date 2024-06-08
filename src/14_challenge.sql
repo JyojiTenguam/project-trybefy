873e7afe381f02b36654b432f2cc7cefb1404984
JyojiTenguam <j.y.oji@hotmail.com>
2024-06-08 01:19:45 -0300

    Resolvendo requisito 15 de Project Trybefy

--- src/14_challenge.sql
+++ src/14_challenge.sql
@@ -1,4 +1,4 @@
---
+-- Escreva uma query que retorne o nome das pessoas artistas e seus respectivos álbuns. Ordene o resultado pelo nome da pessoa em ordem alfabética
 SELECT a.name AS 'Artista', GROUP_CONCAT(albums.title SEPARATOR ',') AS 'Álbuns'
 FROM artists a
     JOIN albums ON a.id = albums.artist_id
---
+++ src/15_challenge.sql
@@ -0,0 +1,2 @@
+-- Escreva uma query para remover álbuns lançados entre 1970 e 1979
+DELETE FROM albums WHERE release_year BETWEEN 1970 AND 1979;
