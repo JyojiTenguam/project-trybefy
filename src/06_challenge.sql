2b6b71f399088bcdbf7f8866b9de8a7f4ea33330
JyojiTenguam <j.y.oji@hotmail.com>
2024-06-07 21:35:13 -0300

    Resolvendo requisito 06 de Project Trybefy

---
+++ src/06_challenge.sql
@@ -0,0 +1,6 @@
+-- Escreva queries para atualizar dados da tabela users
+UPDATE users SET active = 1 WHERE id = 4;
+
+UPDATE users SET active = 0 WHERE id = 8;
+
+UPDATE users set plan_id = 2 WHERE id = 2;
